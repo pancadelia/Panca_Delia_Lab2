a3ab9c891d5b524187a82fa4d7a19a98b7cd92b0
pancadelia <pancadelia98@gmail.com>
2020-10-21 20:22:06 +0300

    laborator3

--- Models/Order.cs
+++ Models/Order.cs
@@ -11,6 +11,7 @@ public class Order
         public int OrderID { get; set; }
         public int CustomerID { get; set; }
         public int BookID { get; set; }
+        public DateTime OrderDate { get; set; }
 
         public Customer Customer { get; set; }
         public Book Book { get; set; }
