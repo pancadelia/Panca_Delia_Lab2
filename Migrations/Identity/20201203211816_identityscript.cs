a9fc99bf31399ccc005939a8117276b6fb303ae6
pancadelia <pancadelia98@gmail.com>
2021-01-07 20:07:34 +0200

    lab11

---
+++ Migrations/Identity/20201203211816_identityscript.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Panca_Delia_Lab2.Migrations.Identity
+{
+    public partial class identityscript : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
