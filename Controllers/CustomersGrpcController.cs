9b13b00c019fd07301f78b5572d13a1583649cc2
pancadelia <pancadelia98@gmail.com>
2021-01-07 20:17:19 +0200

    lab12

--- Controllers/CustomersGrpcController.cs
+++ Controllers/CustomersGrpcController.cs
@@ -17,24 +17,54 @@ namespace Panca_Delia_Lab2.Controllers
             channel = GrpcChannel.ForAddress("https://localhost:5001");
         }
         [HttpGet]
-        public IActionResult Index()
+        pupublic IActionResult Delete(int? id)
         {
+            if (id == null)
+            {
+                return NotFound();
+            }
             var client = new CustomerService.CustomerServiceClient(channel);
-            CustomerList cust = client.GetAll(new Empty());
-            return View(cust);
+            Customer customer = client.Get(new CustomerId() { Id = (int)id });
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
         }
-        public IActionResult Create()
+        public IActionResult Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var client = new CustomerService.CustomerServiceClient(channel);
+            Customer customer = client.Get(new CustomerId() { Id = (int)id });
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
         }
         [HttpPost]
-        public IActionResult Create(Customer customer)
+        public IActionResult DeleteConfirmed(int id)
         {
+            var client = new CustomerService.CustomerServiceClient(channel);
+            Empty response = client.Delete(new CustomerId()
+            {
+                Id = id
+            });
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Edit(int id, Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return NotFound();
+            }
             if (ModelState.IsValid)
             {
-                var client = new
-                CustomerService.CustomerServiceClient(channel);
-                var createdCustomer = client.Insert(customer);
+                var client = new CustomerService.CustomerServiceClient(channel);
+                Customer response = client.Update(customer);
                 return RedirectToAction(nameof(Index));
             }
             return View(customer);
